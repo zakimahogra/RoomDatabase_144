3c09e0b60921caf42fad48590a5bfa1fb93a328b
M S I <M S I@MSI>
2025-11-27 21:09:19 +0700

    membuat penyediaviewmodel

---
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.roomdatabase.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.EntryViewModel
+import com.example.roomdatabase.viewmodel.HomeViewModel
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory{
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
