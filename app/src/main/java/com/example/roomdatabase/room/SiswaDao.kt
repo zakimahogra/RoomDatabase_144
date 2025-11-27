638c985fbc69f7c6f4ff9673125c0984fa4e0d7d
M S I <M S I@MSI>
2025-11-27 21:01:00 +0700

    penambahan kemampuan untuk menyimpan data baru ke database

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -10,3 +10,7 @@ import kotlinx.coroutines.flow.Flow
 interface SiswaDAO {
     @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa() : Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa : Siswa)
+}
