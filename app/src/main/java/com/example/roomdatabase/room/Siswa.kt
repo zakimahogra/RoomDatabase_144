7b99d9f9123b1cba6987493963ba9df1bd543dab
M S I <M S I@MSI>
2025-11-27 20:59:38 +0700

    membuat primarykey dan membuat atribut

--- app/src/main/java/com/example/roomdatabase/room/Siswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/Siswa.kt
@@ -4,4 +4,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 @Entity(tableName = "tblSiswa")
 data class Siswa(
-
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
