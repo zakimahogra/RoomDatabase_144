ee3e3980e333ca0535cc027cff69f0608d3a41b0
M S I <M S I@MSI>
2025-11-27 21:03:03 +0700

    Mendefinisikan Fungsi Komponen Utama Aplikasi (SiswaApp)

---
+++ app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,18 @@
+package com.example.roomdatabase.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.roomdatabase.view.EntrySiswaScreen
+import com.example.roomdatabase.view.HomeScreen
+import com.example.roomdatabase.view.route.DestinasiEntry
+import com.example.roomdatabase.view.route.DestinasiHome
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
