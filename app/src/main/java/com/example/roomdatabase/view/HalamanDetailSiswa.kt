3ee0c03319b4be9f201266360a76ad3b9c28b905
M S I <M S I@MSI>
2025-12-04 16:57:16 +0700

    Implementasi Floating Action Button (FAB) Edit

--- app/src/main/java/com/example/roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/view/HalamanDetailSiswa.kt
@@ -77,3 +77,16 @@ fun DetailSiswaScreen(
         }, modifier = modifier
     ) { innerPadding ->
         val uiState = viewModel.uiDetailState.collectAsState()
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            detailSiswaUiState = uiState.value,
+            onDelete = { coroutineScope.launch {
+                viewModel.deleteSiswa()
+                navigateBack()
+            }},
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
