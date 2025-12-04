6aca6812b71fd129fbf4fe09bf96e4c652599385
M S I <M S I@MSI>
2025-12-04 17:07:21 +0700

    revisi

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,20 +1,33 @@
 package com.example.roomdatabase.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.DetailViewModel
 import com.example.roomdatabase.viewmodel.EntryViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory{
+        // 1. Inisialisasi HomeViewModel
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
+        // 2. Inisialisasi EntryViewModel (HANYA SEKALI)
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        // 3. Inisialisasi DetailViewModel
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
         }
     }
 }
