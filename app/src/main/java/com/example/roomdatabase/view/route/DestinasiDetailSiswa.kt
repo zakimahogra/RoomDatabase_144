1aa81d2ee08d6e1f11b94da00459a15856d3f64d
M S I <M S I@MSI>
2025-12-04 16:51:29 +0700

    Mengimplementasikan Route dengan Argumen

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -6,3 +6,5 @@ object DestinasiDetailSiswa : DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
     const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
