4bab8de468b658c3e42aa8044264bf3e4275e7df
M S I <M S I@MSI>
2025-12-04 16:51:03 +0700

    Menambahkan Konstanta Argumen ID

---
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabase.view.route
+
+import com.example.roomdatabase.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
