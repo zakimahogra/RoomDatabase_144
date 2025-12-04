0b2669d6e421828d7649b84f7b1e4b15711c96ee
M S I <M S I@MSI>
2025-12-04 16:48:32 +0700

    menambahkan delete

--- app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/room/SiswaDao.kt
@@ -1,9 +1,11 @@
 package com.example.roomdatabase.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,4 +15,11 @@ interface SiswaDAO {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa : Siswa)
+
+    @Query(value = "SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa?>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
+
 }
