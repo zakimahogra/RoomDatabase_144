d185f58a933eaa7665e478b478248d51bd6e60bf
M S I <M S I@MSI>
2025-12-04 17:01:43 +0700

    Setup Komponen dan Card Container

--- app/src/main/java/com/example/roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/view/HalamanDetailSiswa.kt
@@ -127,3 +127,15 @@ private fun BodyDetailDataSiswa(
         }
     }
 }
+
+@Composable
+fun DetailDataSiswa(
+    siswa: Siswa, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
