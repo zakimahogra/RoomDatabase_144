eefda6573ef1a494effa4daef2f0689c45ebb4d4
M S I <M S I@MSI>
2025-12-04 17:03:38 +0700

    membuat deleteconfirmationdialog

--- app/src/main/java/com/example/roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/view/HalamanDetailSiswa.kt
@@ -189,3 +189,25 @@ private fun BarisDetailData(
         Text(text = itemDetail, fontWeight = FontWeight.Bold)
     }
 }
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text(stringResource(R.string.attention)) },
+        text = { Text(stringResource(R.string.tanya)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(stringResource(R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(stringResource(R.string.yes))
+            }
+        })
+}
